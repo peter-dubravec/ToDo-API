7e4fdc42ecbe6ab950a33a9a46a0e612e2058497
Peter Dubravec <peter.dubravec98@gmail.com>
2023-04-03 03:14:53 +0200

    Added few comments.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const passport = require("passport")
 const cors = require("cors")
 const swaggerJsDoc = require("swagger-jsdoc")
 const swaggerUi = require('swagger-ui-express')
-
 const swaggerDocument = require('./swagger.json');
 
 // Load enviromental variables
@@ -17,17 +16,14 @@ require("./configs/db.config")
 // Load passport config
 require("./configs/passport.config")(passport)
 
-// swagger config
-const swaggerOptions = require("./configs/swagger.config")
-const swaggerSpec = swaggerJsDoc(swaggerOptions)
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json())
 app.use(cors())
 app.use(logger("dev"))
 
 const indexRouter = require("./routes/indexRouter")(passport)
 app.use("/api/", indexRouter)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT || 5000, (error) => {
     if (!error) {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -9,27 +9,17 @@ const isAuth = require("../middleware/isAuth")
 
 module.exports = (passport) => {
 
-  
+    // Authentification not required for these routes
+
     router.post("/register", userController.register_post)
-
-  
     router.post("/login", userController.login_post)
-
-   
     router.get("/projects", projectController.projects_get)
 
     // Authentification required for these routes
 
-   
     router.post("/create-project", isAuth(passport), projectController.createProject_post)
-
-   
     router.post("/add-task/:projectId", isAuth(passport), taskController.addTask_post)
-
-   
     router.post("/share/:projectId", isAuth(passport), projectController.shareProject_post)
-
-   
     router.post("/change-flag/:projectId/task/:taskId", isAuth(passport), taskController.changeFlag_post)
 
     return router
